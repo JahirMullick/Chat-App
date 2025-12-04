39cd110b7225c5f98318c565f2b16cc2d0127105
MD Jahir Mullick <zahir.mullick@wavers-web.com>
2025-12-04 19:04:26 +0530

    Added NewChatScreen, SettingsScreen

--- src/Navigation/MainStack.tsx
+++ src/Navigation/MainStack.tsx
@@ -1,6 +1,8 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 import ChatScreen from "../screens/ChatScreen";
+import NewChatScreen from "../screens/NewChatScreen";
+import SettingsScreen from "../screens/SettingsScreen";
 import TestScreen from "../screens/TestScreen";
 import DrawerNavigator from "./DrawerNavigator";
 import { MainStackParamList } from "./types";
@@ -18,9 +20,9 @@ export default function MainStack() {
         >
             <Stack.Screen name="Home" component={DrawerNavigator} />
             <Stack.Screen name="Chat" component={ChatScreen} />
+            <Stack.Screen name="NewChat" component={NewChatScreen} />
+            <Stack.Screen name="Settings" component={SettingsScreen} />
             <Stack.Screen name="Test" component={TestScreen} />
-            {/* <Stack.Screen name="Profile" component={ProfileScreen} /> */}
-            {/* <Stack.Screen name="Settings" component={SettingsScreen} /> */}
         </Stack.Navigator>
     );
 }
