037bc41384f3cdcede24abce9749bce59fce778c
MD Jahir Mullick <zahir.mullick@wavers-web.com>
2025-12-04 19:00:37 +0530

    .

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,6 +1,8 @@
 export { default as ForgotPasswordScreen } from "./ForgotPasswordScreen";
 export { default as HomeScreen } from "./HomeScreen";
 export { default as LoginScreen } from "./LoginScreen";
+export { default as NewChatScreen } from "./NewChatScreen";
+export { default as SettingsScreen } from "./SettingsScreen";
 export { default as SignupScreen } from "./SignupScreen";
 export { default as SplashScreen } from "./SplashScreen";
 
