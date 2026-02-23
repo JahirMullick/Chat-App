18c33835ad6a2d5685ffaf84cde1058e08ade38b
MD Jahir Mullick <zahir.mullick@wavers-web.com>
2026-02-23 18:16:12 +0530

    Modified

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import { getMessaging, setBackgroundMessageHandler } from '@react-native-firebase/messaging';
 import { registerRootComponent } from "expo";
+import { LogBox } from 'react-native';
 import App from "./src/App";
+
+// Ignore Firebase namespaced API deprecation warnings until we migrate to modular SDK v22
+LogBox.ignoreLogs([
+    'This method is deprecated (as well as all React Native Firebase namespaced API)',
+]);
 
 // Register background handler (must be done outside of component)
 const messaging = getMessaging();
